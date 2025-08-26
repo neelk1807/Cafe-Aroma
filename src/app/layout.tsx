930d6b24729ec77986b47c77ac8a03343dac0043
neelk <neelk.brainerhub@gmail.com>
2025-08-26 12:21:07 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+
+
 import "./globals.css";
 import ShowLayoutWrapper from "./component/ShowLayoutWrapper";
 
@@ -28,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ShowLayoutWrapper>{children}</ShowLayoutWrapper>
+        <Analytics />
       </body>
     </html>
   );
